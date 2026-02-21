0efaffb3f60c22b02bc971e75709919caaa97e1b
Paraspandey-debugs <p.pandey250806@gmail.com>
2026-02-21 16:55:04 +0530

    feat(internal/core): added support for different status code and also added backoff

--- internal/core/download.go
+++ internal/core/download.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -21,6 +24,10 @@ func main() {
 	}
 }
 
+const maxRetries = 10
+const baseTime = 1 * time.Second
+const maxBackoff = 32 * time.Second
+
 func DownloadFile(url string, filepath string) error {
 	out, err := os.Create(filepath)
 	if err != nil {
@@ -28,15 +35,41 @@ func DownloadFile(url string, filepath string) error {
 	}
 	defer out.Close()
 
-	resp, err := http.Get(url)
+	client := &http.Client{
+		Timeout: 15 * baseTime,
+	}
+	var resp *http.Response
+	//backoff logic writtne idk why but good practice
+	for i := 0; i < maxRetries; i++ {
+		resp, err = client.Get(url)
+		//response handeling
+		if err != nil {
+			if i != maxRetries-1 {
+				return err
+			}
+		} else if resp.StatusCode >= 500 || resp.StatusCode == http.StatusRequestTimeout {
+			resp.Body.Close()
+			if i == maxRetries-1 {
+				return fmt.Errorf("server error : %d", resp.StatusCode)
+			}
+		} else {
+			break
+		}
+
+		backoff := time.Duration(math.Min(
+			float64(baseTime)*math.Pow(2, float64(i)),
+			float64(maxBackoff),
+		))
+		jitter := time.Duration(rand.Float64() * float64(backoff) * 0.5)
+		time.Sleep(backoff + jitter)
+	}
 	if err != nil {
 		return err
 	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status: %s", resp.Status)
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("request failed: %d", resp.StatusCode)
 	}
+	defer resp.Body.Close()
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
